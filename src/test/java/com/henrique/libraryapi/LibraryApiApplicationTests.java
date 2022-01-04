f16a26059cd72e645d502d9729eb1aa9ac1db6f5
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-01-03 21:24:50 -0300

    Iniciando api livraria

---
+++ src/test/java/com/henrique/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
